a454a2e004a3ca5d87b018b4f870bf15f25eb199
beatrizbramont <beatriz.bramont@gmail.com>
2025-10-25 00:05:46 -0300

    delete html,css,js puro

--- frontend/js/main.js
+++
@@ -1,9 +0,0 @@
-function bloquearHome(){
-    const homeLink = document.getElementById("home")
-    homeLink.addEventListener('click', function(event){
-        event.preventDefault()
-        alert("⚠️ Para acessar é necessário estar logado")
-    })
-}
-
-bloquearHome()
